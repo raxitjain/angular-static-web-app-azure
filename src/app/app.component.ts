047082a5298c348cb8e452b12434240315ff3f1a
Raxit Jain <raxit.s.jain@gds.ey.com>
2021-04-01 12:22:07 +0530

    Integrated angular static web app with azure functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: `app.component.html`,
 })
 export class AppComponent {
-  value = 'World';
+
+  message: string;
+  name: string;
+
+  async fetchMessage() {
+    if (this.name) {
+      const response = await fetch('https://rj-azure-function.azurewebsites.net/api/message/', {
+        method: 'POST',
+        body: JSON.stringify({ name: this.name })
+      });
+      const responseBody = await response.json();
+      this.message = responseBody.message;
+      this.name = '';
+    }
+  }
+
 }
